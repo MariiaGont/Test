899f90108a5daf2cf04664ce829a024849e64766
Mariia <mari.gontareva@inbox.ru>
2023-01-11 06:52:25 +0700

    Task solution added

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        // Написать программу, которая из имеющегося массива строк формирует массив из строк, длина которых меньше либо равна 3 символа.
+        void Task()
+        {
+            Console.WriteLine("Программа принимает на вход строки и выдаёт те из них, длина которых меньше или равна 3 символам.");
+            Console.Write("Введите строки через пробел: ");
+            string lines = Console.ReadLine();
+            string[] input = lines.Split();
+            int inputLength = input.Length;
+            string[] output = new string[inputLength];
+            int stringLength = 3;
+            Console.WriteLine();
+
+            for (int i = 0; i < inputLength; i++)
+            {
+                int j = 0;
+                if (input[i].Length <= stringLength)
+                {
+                    output[j] = input[i];
+                    Console.Write(output[j] + " ");
+                    j++;
+                }
+            }
+            Console.WriteLine(" ");
+        }
+        Task();
+    }
+}
